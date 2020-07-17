7a3fa262d4ce0844b6c4f5618095aa885d9925d7
Gregory Bastianelli <greg@gerrg.com>
2020-07-17 12:32:17 -0400

    Add win conditions

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const playerFactory = ( symbol ) => {
+      /**
+       * Player Object.
+       */
+      
       let wins = 0;
 
       const win = () => { wins++; }
-      return { wins, symbol }
+
+      return { wins, symbol, win }
 }
 
 const ticTacToe = (() => {
 
+      // dom elements
       var cells = document.getElementsByTagName('td');
+
+      // init variables
+      var gameIsOver = false;
       var board = [ '', '', '', '', '', '', '', '', ''];
       var turn = 0;
 
+      //setup players
       var players = {
             'x': playerFactory('x'),
             'o': playerFactory('o')
@@ -18,63 +28,134 @@ const ticTacToe = (() => {
 
 
 
+      /**
+       * Runs when a cell is clicked - decides whose turn it is, reflects the event and checks for winner
+       * @param {event}
+       */
       const move = ( e ) => {
-            /**
-             * Updates the DOM if the player makes a legal move.
-             */
+
+            // get whose turn it is
             let currentPlayer = whoseTurnIsItAnyways();
 
+            // the cell last clicked
+            let index = e.target.id;
 
-            if( ! gameOver() && board[e.target.id] === '' ){
-                  board[e.target.id] = currentPlayer.symbol;
-                  render()
+            // if the game isnt over and the selected cell not already filled in
+            if( ! gameIsOver ){
 
-                  turn++;
-                  updateTurnCounter()
-            }
+                  if( board[index] === '' ){
+
+                        // set array value
+                        board[index] = currentPlayer.symbol;
+      
+                        // show the board, update UI
+                        updateTheDom()
+      
+                        // returns if any of the win conditions have been met - otherwise we just keep playing.
+                        gameIsOver = checkForWinner();
+                  }
+                  
+            } 
 
       }
 
-      const render = () => {
+      const updateTheDom = () => {
             /**
-             * Take the board and put it in the DOM
+             * Loop loops through the board and matches it's respective DOM element with whats in the array.
              */
             for( let i = 0; i < cells.length; i++ ){
                   cells[i].innerText = board[i]
             }
+
+            updateTurnCounter()
+            
       }
 
+      /**
+       * Updates the turn count and reflects that on the DOM
+       */
       const updateTurnCounter = () => {
+            turn++;
             document.getElementById('turn-counter').innerHTML = turn;
       }
 
-      const whoseTurnIsItAnyways = () => {
-            return ( turn % 2 === 0 ) ? players['x'] : players['o'];
-      }
+      /**
+       * Checks whose turn it is by checking whether the turn count is odd or even
+       * @return {playerFactory}
+       */
+      const whoseTurnIsItAnyways = () => ( turn % 2 === 0 ) ? players['x'] : players['o'];
+      
 
+      /**
+       * Setup column array - loop and check for win
+       * @return {bool}
+       */
       const isVerticalWin = () => {
-            return false;
+            let columns = [
+                  [ board[0], board[3], board[6] ],
+                  [ board[1], board[4], board[7] ],
+                  [ board[2], board[5], board[8] ]
+            ]
+            
+            return loopNcheck( columns )
       }
 
+
+      /**
+        * Setup rows array - loop and check for win
+        * @return {bool} 
+        */
       const isHorizontalWin = () => {
-            return false;
+            let rows = [
+                  [ board[0], board[1], board[2] ],
+                  [ board[3], board[4], board[5] ],
+                  [ board[6], board[7], board[8] ]
+            ]
+            
+            return loopNcheck( rows )
       }
 
+      /**
+       * Setup diagnoals array - loop and check for win
+       * @return {bool}
+       */
       const isDiagonalWin = () => {
-            return false;
+            let diagonals = [
+                  [ board[0], board[4], board[8] ],
+                  [ board[6], board[4], board[2] ],
+            ]
+            
+            return loopNcheck( diagonals )
       }
 
-      const isDraw = () => {
-            /**
-             * Check for whether or not the game has exceeded the maximum number of legal turns in a game of Tic-Tac-Toe
-             */
-            return ( turn > 9 )
+      /**
+       * Loop through each array and check if columns/rows/diagonals all match each other.
+       * @param {array} arrays 
+       */
+      const loopNcheck = (arrays) => {
+
+            for( let array of arrays ){
+                  if( allEqualAndNotBlank( array ) ) { return [ true, array ] }
+            }
+
+            return false
       }
 
-      const gameOver = () => {
-            /**
-             * Checks if the game is over by running down a list of winning strategies.
-             */
+       /**
+       * Checks if the array given is equal to the first element and also not blank.
+       * https://stackoverflow.com/a/35568895/3247137
+       */
+      const allEqualAndNotBlank = arr => arr.every( v => v === arr[0] && arr[0] != '' )
+
+      /**
+       * Check for whether or not the game has exceeded the maximum number of legal turns in a game of Tic-Tac-Toe
+       */
+      const isDraw = () => (turn > 9)
+
+      /**
+       * Checks if the game is over by running down a list of winning strategies.
+       */
+      const checkForWinner = () => {
 
              // ways to win
             let winTypes = [
@@ -86,14 +167,18 @@ const ticTacToe = (() => {
 
             // Run each function and break if any are true.
             for( let checkForWin of winTypes ){
-                  if( checkForWin() ){ return true; }
+                  
+                  // if there is a winner 
+                  if( checkForWin() ) { 
+                        // stop game
+                        return true;
+                  }
             }
 
-            // if we make it here - the game is still going
             return false;
-            
+
       }
-      
+
       // listeners
       for( let cell of cells  ){
             cell.addEventListener('click', move)
